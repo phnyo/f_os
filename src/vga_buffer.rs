718a25200ebbccc9c51f903bb303be7ee0f10ef2
n <sabanium@gmail.com>
2021-12-28 11:58:16 +0900

    created serial.rs to send message to the console, updated cargo toml for testing, implemented simple tests

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -123,7 +123,7 @@ use spin::Mutex;
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Black, Color::Cyan),
+        color_code: ColorCode::new(Color::LightRed, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
