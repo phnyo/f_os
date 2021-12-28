8deabeb5411eb039efa44d792e3770c513c0c480
n <sabanium@gmail.com>
2021-12-28 11:06:25 +0900

    implemented print! println! macro and spinlock for safe access on the thread

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,17 @@
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello, World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    
-    vga_buffer::print_something();
-
+    println!("Hello, World!");
+    panic!("Meow! there is a cat in the crate(really!?)");
     loop {}
 }
 
